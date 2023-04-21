d970dbdd53cbca3884b43a92ea5679623bb94f73
Juan Mazuera 2043008 <81594863+JuanYunda@users.noreply.github.com>
2023-04-21 14:50:05 -0500

    rama creada para las pruebas

--- src/models/YoutubeVideo.jsx
+++ src/models/YoutubeVideo.jsx
@@ -1,27 +1,32 @@
-import React, { useRef } from 'react';
-import { useFrame } from '@react-three/fiber';
-import { Html } from '@react-three/drei';
-import * as THREE from 'three';
-import YouTube from 'react-youtube';
+import { Html } from "@react-three/drei";
+import { useRef } from "react";
+import { useVideo } from "react-use";
 
 export default function YoutubeVideo(props) {
-  const meshRef = useRef();
-  const videoId = 'FXlE4EHm1lw';
-
-  useFrame(() => {
-    meshRef.current.material.map.needsUpdate = true;
-  });
+  const planeRef = useRef();
+  const videoUrl = "https://www.youtube.com/embed/uNvaiooJDRA";
+  const [video] = useVideo(videoUrl);
 
   return (
     <group {...props} dispose={null}>
-      <mesh ref={meshRef}>
+      <mesh ref={planeRef}>
         <planeBufferGeometry args={[16, 9]} />
-        <meshBasicMaterial map={new THREE.Texture()} />
-        <Html style={{ width: '100%', height: '100%' }}>
-          <div style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)' }}>
-            <YouTube videoId={videoId} />
-          </div>
-        </Html>
+        <meshBasicMaterial>
+          <Html>
+            {video && (
+              <video
+                ref={video.ref}
+                autoPlay={true}
+                loop
+                muted
+                crossOrigin="anonymous"
+                style={{ width: "100%", height: "100%" }}
+              >
+                <source src={video.src} type="video/mp4" />
+              </video>
+            )}
+          </Html>
+        </meshBasicMaterial>
       </mesh>
     </group>
   );
